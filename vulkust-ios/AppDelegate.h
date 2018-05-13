1b46cf0bc3adce8895fbdb84903aa7ecbb4c2daa
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2018-05-13 21:00:08 +0430

    Initial Commit

---
+++ vulkust-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vulkust-ios
+//
+//  Created by Hossein Noroozpour on 5/13/18.
+//  Copyright © 2018 Gearoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
