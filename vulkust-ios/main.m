4ba33dd5d261291f457a5cf2d40b6434ab4c3dca
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2018-05-14 12:29:06 +0430

    initial commit

--- vulkust-ios/main.m
+++ vulkust-ios/main.m
@@ -1,16 +1,5 @@
-//
-//  main.m
-//  vulkust-ios
-//
-//  Created by Hossein Noroozpour on 5/13/18.
-//  Copyright © 2018 Gearoenix. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
-
+void vulkust_main(int argc, char ** argv);
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+    vulkust_main(argc, argv);
+    return 0;
 }
