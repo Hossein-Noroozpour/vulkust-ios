f29b535376b7ec1e7df69e8e1c58bd3b95920d22
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2018-05-17 10:00:34 +0430

    vulkan is now working smoothly

--- vulkust-ios/vulkust.h
+++ vulkust-ios/vulkust.h
@@ -1,9 +1,9 @@
 #ifndef vulkust_h
 #define vulkust_h
-
+extern "C" {
 void * vulkust_allocate(void);
 void vulkust_deallocate(void* ctx);
 void vulkust_set_view(void* ctx, void* view);
 void vulkust_render(void* ctx);
-
+}
 #endif
